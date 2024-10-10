6369a3a5ae0f36e476e60506a0f497fe223fdaf8
Joao-antonio-gg <joaoagg@al.insper.edu.br>
2024-10-10 09:37:44 -0300

    Inicial

--- main.py
+++ main.py
@@ -1,14 +1,50 @@
-from typing import Optional
-
-from fastapi import FastAPI
+from typing import Union
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import FileResponse
+from PIL import Image
+from ultralytics import YOLO
+import numpy
+import os
 
 app = FastAPI()
 
+# Configuração do CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],  # Permite a origem do Next.js
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+model = YOLO("treinamento/best.pt")
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def read_root():
+    return {"Hello": "World"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.post("/image/")
+async def upload_image(file: UploadFile):
+    if file is None:
+        return {"error": "No file"}
+
+    try:
+        image = Image.open(file.file)
+        result = model(image)
+        name = result[0].names
+        probs = result[0].probs.data.numpy()
+        result_f = (name[numpy.argmax(probs)])
+
+        # bloqueios = [FileResponse("img/cadeado.jpg", media_type="image/jpg"), FileResponse("img/chave.jpg", media_type="image/jpg")]
+        # if result_f != 'chave':
+        #     bloqueios.append (FileResponse("img/"+ result_f + ".jpg", media_type="image")) 
+        bloqueios = ['cadeado', 'etiqueta']
+        if result_f != 'chave':
+            bloqueios.append(result_f)
+
+        return {"result": result_f, "bloqueios": bloqueios}
+    
+
+    except Exception as e:
+        return {"error": str(e)}
